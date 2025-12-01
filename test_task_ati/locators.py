b6bdf5655f5b141b24b64fa9be3bd6275e2b53d8
Viktoria94 <vikkoles13@gmail.com>
2025-12-01 10:55:16 +0300

    fix(locators) add locator ffast reg title, fix locator search result

--- test_task_ati/locators.py
+++ test_task_ati/locators.py
@@ -13,6 +13,7 @@ class TrucksPageLocators(object):
     INPUT_TO = (By.XPATH, "(//*[@autocomplete or contains(@class,'autocomplete')])[2]")
     DROPDOWN_MENU_TO = (By.ID, "react-autowhatever-to--item-0")
     BUTTON_SEARCH_TRUCKS = (By.XPATH, "//button[normalize-space()='найти транспорт']")
-    SEARCH_RESULTS = (By.XPATH, "//div[@data-qa='total-trucks-count']")
+    SEARCH_RESULTS = (By.CSS_SELECTOR, "[data-qa='total-trucks-count']")
     SHOW_CONTACTS = (By.XPATH, "(//button[normalize-space()='Показать контакты'])[10]")
     POP_UP_FAST_REG = (By.XPATH, "//div[@class='ati-id-login']")
+    POPUP_FAST_REG_TITLE = (By.XPATH, "//h1[normalize-space()='Вход на ATI.SU']")
